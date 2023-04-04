75279e8681718a6e35361760dbc1ff68f590c965
Yasin051197 <yasinjamadar0511@gmail.com>
2023-04-04 11:29:02 +0530

    added files

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -47,40 +47,44 @@ usersRoute.post("/login",async(req,res)=>{
         const user=await User.find({email:email});
         
         if(user.length>0){
-            if(user[0].login_attempts<=5 && user[0].lock<=24){
+            if(user[0].lock===null){
 
-            bcrypt.compare(password, user[0].password, async(err,result)=> {
+              bcrypt.compare(password, user[0].password, async(err,result)=> 
+              {
                 if(result){
                     await User.findByIdAndUpdate(user[0]._id,{login_attempts:0,lock:null})
                     res.send({msg:"Login Successful",email:user[0].email})
                 }
                 else{
-
                     await User.findByIdAndUpdate(user[0]._id,{login_attempts:user[0].login_attempts+1})
             
-                    if(user[0].login_attempts>=5){
-                        const d= 1000*60*60*24;
-            
-                        const lt=new Date().getTime() + d;
-            
-                        await User.findByIdAndUpdate(user[0]._id,{lock:new Date(lt)});   
+                      if(user[0].login_attempts>5){
+                           const tt=new Date()
+                           const lt=tt.getTime()+1000*60*60*24;
+                           console.log(lt)
+                           await User.findByIdAndUpdate(user[0]._id,{lock:lt});   
+                        }
+                        res.send({msg:"Login Failed, plz enter correct credintials"});
                     }
-                    res.send({msg:"Login Failed, plz enter correct credintials"});
+               });
+            }
+            else{
+                const tt=new Date()
+                const checktime=tt.getTime()
+                if(checktime===user[0].lock)
+                {
+                    await User.findByIdAndUpdate(user[0]._id,{login_attempts:0,lock:null})
+                    res.send({msg:"Login Successful",email:user[0].email})
                 }
-            });
-        }
-        else{
-            let time=24;
-            await User.findByIdAndUpdate(user[0]._id,{lock:time})
-
-            res.send({msg:`U have been blocked for 24 hours} `})
+                else{
+                     res.send({msg:`U have been blocked for ${user[0].lock} hours} `})
+                }
         }
     }
     else{
         res.send({msg:"U r not user please login first"})
     }
-}
-    catch(err){
+}catch(err){
         res.send(err);
     }
 })
